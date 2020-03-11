c657547c037864befd54e28e7293b58b11f4f32e
apitoong <apitoong.development@gmail.com>
2020-03-11 16:51:23 +0700

    push git

--- Outlets+Actions/CalculatorViewController.swift
+++ Outlets+Actions/CalculatorViewController.swift
@@ -12,6 +12,9 @@ class CalculatorViewController: UIViewController {
 
     @IBOutlet weak var priceValue: UITextField!
     @IBOutlet weak var sellPrice: UILabel!
+    @IBOutlet weak var costomValue: UISlider!
+    var count = ""
+    @IBOutlet weak var countView: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,10 +23,34 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func getlow(_ sender: Any) {
-        let val=Double(priceValue.text ?? "10") ?? 12
+        costomValue.value=0.25
+        let val=Double(priceValue.text ?? "10") ?? 10
         let dist  = val*1/4+val
         sellPrice.text = "labanya " + String (dist)
         print(dist)
     }
     
+    @IBAction func getHight(_ sender: Any) {
+        costomValue.value=1.0
+        let val=Double(priceValue.text ?? "10") ?? 12
+        let dist  = val*3/4+val
+        sellPrice.text = "labanya " + String (dist)
+        print(dist)
+    }
+    @IBAction func getMed(_ sender: Any) {
+        costomValue.value=0.5
+        let val=Double(priceValue.text ?? "10") ?? 10
+        let dist  = val*2/4+val
+        sellPrice.text = "labanya " + String (dist)
+        print(dist)
+    }
+    @IBAction func getCostom(_ sender: Any) {
+         
+//        print(costomValue.value * 100)
+        let val=Double(priceValue.text ?? "10") ?? 10
+        count = String(Int(costomValue.value * 100) )
+        countView.text = count + " %"
+        let result = val + (val * (Double(count ) ?? 0.5))/100.0
+        sellPrice.text = "labanya " + String(result )  
+    }
 }
